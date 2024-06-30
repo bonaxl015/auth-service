602d1ffad2f8acbf5297e5b4df33314c7b7d4d17
bonaxl015 <bonaxl015@gmail.com>
2024-06-30 21:00:22 +0800

    feat: user controllers

---
+++ src/api/controllers/user/index.ts
@@ -0,0 +1,82 @@
+import { Request, Response, NextFunction } from 'express';
+import userService from '@db/services/UserService';
+import asyncHandler from '@middlewares/global/async-handler';
+import ErrorResponse from '@utils/errorResponse';
+import ResponseCodes from '@enums/responseCodes';
+import { BaseError } from 'sequelize';
+import { CreateUserInput, UpdateUserInput } from '@db/types/user.types';
+
+export const getUserById = asyncHandler(
+  async (
+    req: Request,
+    res: Response,
+    _next: NextFunction,
+  ): Promise<void | Response> => {
+    const id: number = Number(req.params.id);
+    const results = await userService.getById(id);
+
+    if (results instanceof ErrorResponse) {
+      throw results;
+    }
+
+    return res.status(ResponseCodes.SUCCESS).send(results);
+  },
+);
+
+export const createUser = asyncHandler(
+  async (
+    req: Request,
+    res: Response,
+    _next: NextFunction,
+  ): Promise<void | Response> => {
+    const payload: CreateUserInput = req.body;
+    const results = await userService.create(payload);
+
+    if (results instanceof ErrorResponse) {
+      throw results;
+    }
+
+    return res.status(ResponseCodes.SUCCESS).send(results);
+  },
+);
+
+export const updateUser = asyncHandler(
+  async (
+    req: Request,
+    res: Response,
+    _next: NextFunction,
+  ): Promise<void | Response> => {
+    const id: number = Number(req.body.id);
+    const payload: UpdateUserInput = req.body;
+
+    const results = await userService.update(id, payload);
+
+    if (results instanceof BaseError) {
+      throw results;
+    }
+
+    return res.status(ResponseCodes.SUCCESS).send(results);
+  },
+);
+
+export const deleteUser = asyncHandler(
+  async (
+    req: Request,
+    res: Response,
+    _next: NextFunction,
+  ): Promise<void | Response> => {
+    const id: number = Number(req.body.id);
+
+    const results = await userService.deleteById(id);
+
+    if (results instanceof ErrorResponse) {
+      throw new ErrorResponse(
+        results.name,
+        results.message,
+        ResponseCodes.NOT_FOUND,
+      );
+    }
+
+    return res.status(ResponseCodes.SUCCESS).send(results);
+  },
+);
